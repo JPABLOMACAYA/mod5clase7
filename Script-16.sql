ef4741e43436ad2076b1d20aeaf1d3b6c52abe4e
JPABLOMACAYA <103909266+JPABLOMACAYA@users.noreply.github.com>
2022-06-25 19:10:23 -0400

    Update Script-16.sql

--- Script-16.sql
+++ Script-16.sql
@@ -1,7 +1,7 @@
 create table cliente (
 	rut varchar(10),
 	nombre varchar(50),
-	dirección varchar(50),
+	direcciÃ³n varchar(50),
 	primary key (rut)
 );
 
@@ -16,22 +16,22 @@ create table factura (
 	foreign key (rut_cliente) references cliente(rut)
 );
 
-create table categoría (
+create table categorÃ­a (
 	id int,
 	nombre varchar(30),
-	descripción varchar(60),
+	descripciÃ³n varchar(60),
 	primary key (id)
 );
 
 create table producto (
 	id int,
 	nombre varchar(40),
-	descripción varchar(60),
+	descripciÃ³n varchar(60),
 	stock int,
 	precio_unitario int,
-	id_categoría int,
+	id_categorÃ­a int,
 	primary key (id),
-	foreign key (id_categoría) references categoría(id)
+	foreign key (id_categorÃ­a) references categorÃ­a(id)
 );
 
 create table factura_producto (
@@ -46,24 +46,24 @@ create table factura_producto (
 
 select * from factura_producto;
 select * from producto;
-select * from categoría;
+select * from categorÃ­a;
 select * from factura;
 select * from cliente;
 
 insert into cliente values 
-	('11111111-2', 'Andrés Ramírez', 'Calle 1, Chillán'),
-	('12222222-5', 'Patricio Bustos', 'Pasaje 2, Chillán'),
-	('13333333-k', 'Andrés Soto', 'Calle 7, Chillán Viejo');
+	('11111111-2', 'AndrÃ©s RamÃ­rez', 'Calle 1, ChillÃ¡n'),
+	('12222222-5', 'Patricio Bustos', 'Pasaje 2, ChillÃ¡n'),
+	('13333333-k', 'AndrÃ©s Soto', 'Calle 7, ChillÃ¡n Viejo');
 
-insert into categoría values 
-	(200301, 'Monitores', 'Monitores de todos los tamaños y marcas'),
-	(500101, 'Teclados', 'Teclados de todos los tamaños y marcas');
+insert into categorÃ­a values 
+	(200301, 'Monitores', 'Monitores de todos los tamaÃ±os y marcas'),
+	(500101, 'Teclados', 'Teclados de todos los tamaÃ±os y marcas');
 
 insert into producto values 
 	(200301005, 'Monitor de 20 pulgadas Samsung', 'Pantalla LED Full HD', 50, 2000, 200301),
 	(200301007, 'Monitor de 27 pulgadas MSI', 'Pantalla LED UHD', 300, 3000, 200301),
 	(200301012, 'Monitor de 22 pulgadas Asus', 'Pantalla LED Full HD', 100, 3500, 200301),
-	(500101001, 'Teclado wireless oficina Logitech', 'Teclado inalámbrico', 80, 45000, 500101),
+	(500101001, 'Teclado wireless oficina Logitech', 'Teclado inalÃ¡mbrico', 80, 45000, 500101),
 	(500101002, 'Teclado gamer Corsair', 'Teclado cableado usb', 40, 40000, 500101);
 
 begin;
@@ -94,12 +94,12 @@ on cliente.rut = factura.rut_cliente
 where factura.monto_total = (select max(monto_total) from factura);
 
 --Item 6
-select nombre, monto_total as compra_más_de_60$  --60 es un monto muy bajo, se repetirá más abajo con monto más grande
+select nombre, monto_total as compra_mÃ¡s_de_60$  --60 es un monto muy bajo, se repetirÃ¡ mÃ¡s abajo con monto mÃ¡s grande
 from cliente * join factura
 on cliente.rut = factura.rut_cliente
 where factura.monto_total >= 60;
 
-select nombre, monto_total as compra_más_de_$80mil --aquí se aprecia que logra omitir los montos menores a $80.000.-
+select nombre, monto_total as compra_mÃ¡s_de_$80mil --aquÃ­ se aprecia que logra omitir los montos menores a $80.000.-
 from cliente * join factura
 on cliente.rut = factura.rut_cliente
 where factura.monto_total >= 80000;
@@ -112,7 +112,7 @@ join cliente on cliente.rut = factura.rut_cliente
 group by cliente.nombre
 order by clientes_compran_mas_de_5_unid desc;
 
--- Luego se identifica a los clientes con más de 5 unidades compradas totales (en este caso los 3 clientes cumplen el requisito)
+-- Luego se identifica a los clientes con mÃ¡s de 5 unidades compradas totales (en este caso los 3 clientes cumplen el requisito)
 select cliente.nombre, sum(factura_producto.cantidad_producto) > 5 as clientes_compran_mas_de_5_unid from factura
 join factura_producto on factura.nro_factura = factura_producto.nro_factura
 join cliente on cliente.rut = factura.rut_cliente
@@ -120,7 +120,7 @@ group by cliente.nombre
 order by clientes_compran_mas_de_5_unid desc;
 
 -- Luego se cuenta a aquellos clientes que cumplieron con el requisito
--- Método A (usando condición booleana en where y "mayor que" en sum)
+-- MÃ©todo A (usando condiciÃ³n booleana en where y "mayor que" en sum)
 select count(clientes_compran_mas_de_5_unid) as cant_clientes_mas_de_5_unids
 from (select cliente.nombre, sum(factura_producto.cantidad_producto) > 5 as clientes_compran_mas_de_5_unid from factura
 join factura_producto on factura.nro_factura = factura_producto.nro_factura
@@ -129,7 +129,7 @@ group by cliente.nombre
 order by clientes_compran_mas_de_5_unid desc) as tabla_temporal
 where clientes_compran_mas_de_5_unid is true;
 
--- Método B (usando sólo condición "mayor que" en where)
+-- MÃ©todo B (usando sÃ³lo condiciÃ³n "mayor que" en where)
 select count(clientes_compran_mas_de_5_unid) as cant_clientes_mas_de_5_unids
 from (select cliente.nombre, sum(factura_producto.cantidad_producto) as clientes_compran_mas_de_5_unid from factura
 join factura_producto on factura.nro_factura = factura_producto.nro_factura
@@ -139,7 +139,7 @@ order by clientes_compran_mas_de_5_unid desc) as tabla_temporal
 where clientes_compran_mas_de_5_unid > 5;
 
 -- Ejercicio adicional, aumentando las unidades, para que algunos clientes queden fuera.
--- Para el caso de clientes con más de 25 unidades compradas totales (en este caso sólo 1 cliente cumple el requisito)
+-- Para el caso de clientes con mÃ¡s de 25 unidades compradas totales (en este caso sÃ³lo 1 cliente cumple el requisito)
 select cliente.nombre, sum(factura_producto.cantidad_producto) > 25 as clientes_compran_mas_de_25_unid from factura
 join factura_producto on factura.nro_factura = factura_producto.nro_factura
 join cliente on cliente.rut = factura.rut_cliente
@@ -147,7 +147,7 @@ group by cliente.nombre
 order by clientes_compran_mas_de_25_unid desc;
 
 -- Luego se cuenta a aquellos clientes que cumplieron con el requisito
--- Método A (usando condición booleana en where y "mayor que" en sum)
+-- MÃ©todo A (usando condiciÃ³n booleana en where y "mayor que" en sum)
 select count(clientes_compran_mas_de_25_unid) as cant_clientes_mas_de_25_unids
 from (select cliente.nombre, sum(factura_producto.cantidad_producto) > 25 as clientes_compran_mas_de_25_unid from factura
 join factura_producto on factura.nro_factura = factura_producto.nro_factura
@@ -156,7 +156,7 @@ group by cliente.nombre
 order by clientes_compran_mas_de_25_unid desc) as tabla_temporal
 where clientes_compran_mas_de_25_unid is true;
 
--- Método B (usando sólo condición "mayor que" en where)
+-- MÃ©todo B (usando sÃ³lo condiciÃ³n "mayor que" en where)
 select count(clientes_compran_mas_de_25_unid) as cant_clientes_mas_de_25_unids
 from (select cliente.nombre, sum(factura_producto.cantidad_producto) as clientes_compran_mas_de_25_unid from factura
 join factura_producto on factura.nro_factura = factura_producto.nro_factura
@@ -164,79 +164,3 @@ join cliente on cliente.rut = factura.rut_cliente
 group by cliente.nombre
 order by clientes_compran_mas_de_25_unid desc) as tabla_temporal
 where clientes_compran_mas_de_25_unid > 25;
-
-/*
-Otros intentos de soluciones--->
-
-select cliente.nombre, sum(factura_producto.cantidad_producto) as unidades_compradas from factura
-join factura_producto on factura.nro_factura = factura_producto.nro_factura
-join cliente on cliente.rut = factura.rut_cliente
-group by cliente.nombre
-order by unidades_compradas desc;
-
-select count(cliente.nombre) as nro_clientes_mas_5_unidades from cliente;
-
-select count(cliente.nombre) from factura
-join factura_producto on factura.nro_factura = factura_producto.nro_factura
-join cliente on cliente.rut = factura.rut_cliente
-where factura_producto.cantidad_producto > 10
-
-select cliente.nombre, (select sum(factura_producto.cantidad_producto) as unidades_compradas) from factura
-join factura_producto on factura.nro_factura = factura_producto.nro_factura
-join cliente on cliente.rut = factura.rut_cliente
-group by cliente.nombre
-order by unidades_compradas desc;
-
-select cliente.nombre, count(factura_producto.cantidad_producto) as compras_mayores_5_unid from factura
-join factura_producto on factura.nro_factura = factura_producto.nro_factura
-join cliente on cliente.rut = factura.rut_cliente
-where cantidad_producto > 5
-group by cliente.nombre
-order by compras_mayores_5_unid desc;
-
-select sum(factura_producto.cantidad_producto), count(cliente.nombre) as clientes_compran_mas_de_5_unid from factura
-join factura_producto on factura.nro_factura = factura_producto.nro_factura
-join cliente on cliente.rut = factura.rut_cliente
-where cantidad_producto > 5
-group by cliente.nombre
-order by clientes_compran_mas_de_5_unid desc;
-
-/*
---Estas facturas deben ingresarse mediante transacciones, por lo tanto se verá afectado el stock de cada producto
-insert into factura values 
-	(001, '2022-06-15', 85000, 16150, 101150, '11111111-2'),
-	(002, '2022-06-16', 425000, 80750, 505750, '12222222-5'),
-	(003, '2022-06-18', 60000, 11400, 71400, '13333333-k');
-	
-insert into factura_producto values 
-	(001, 200301005, 10, 20000),
-	(001, 200301007, 10, 30000),
-	(001, 200301012, 10, 35000),
-	(002, 500101001, 5, 225000),
-	(002, 500101002, 5, 200000),
-	(003, 200301007, 20, 60000);
-*/
-
--- Otro ejemplo de transacciones. Aún se podría pulir más, por ej. utilizando la función SUM en los totales de c/producto
---para obtener el subtotal de la factura. 
-begin;
-	insert into factura values (001, '2022-06-15', null, null, null, null);
-	insert into factura_producto values	(001, 200301005, 10, (select producto.precio_unitario from producto where producto.id = 200301005)*10), (001, 200301007, 10, (select producto.precio_unitario from producto where producto.id = 200301007)*10), (001, 200301012, 10, (select producto.precio_unitario from producto where producto.id = 200301012)*10);
-	update factura set rut_cliente = '11111111-2' where nro_factura = 001;
-	update producto set stock = stock - 10 where id = 200301005;
-	update producto set stock = stock - 10 where id = 200301007;
-	update producto set stock = stock - 10 where id = 200301012;
-commit;
-*/
---falta insertar inicialmente a la factura sólo el id y el cliente, lo demás null.
---luego de insertar valores a factura_producto, realizar update a factura con los valores faltantes faltantes
---para hacer lo anterior. primero deberé realizar delete a los datos ya ingresados en ambas tablas involucradas
-
-/*
-drop table factura_producto;
-drop table producto;
-drop table categoría;
-delete from factura;
-drop table factura;
-drop table cliente;
-*/
